451a2c9e97ea420ef7ada51c8b5b6549cb9840b9
Louis Muhammad <louis.muhammad@gmail.com>
2013-11-13 20:40:16 -0500

    Deployment config files

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/sapdeployer/apps/pathways/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.pathways.sock"
+worker_processes 2
+timeout 30
